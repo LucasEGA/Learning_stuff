124c5a7ffee1a2c0dcabd0d2bdf303d7c750d9f2
Lucas Grimm Aguilera <Lucas230208@icloud.com>
2025-09-11 17:21:54 +0200

    Update binary_search.java

--- Java/search algorithm/binary_search.java
+++ Java/search algorithm/binary_search.java
@@ -1,1 +1,24 @@
-
+public class Main{
+    static int binary_search(int numbers[], int target)
+    {
+        int left = 0;
+        int right = numbers.length - 1; 
+        while (left <= right)
+        {
+             int mid = (left + right) / 2;
+            if (numbers[mid] == target)
+            {
+                return mid;
+            }
+             if (numbers[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+            left = mid + 1 ;    
+            } 
+        }
+        return -1;
+    }
+}
