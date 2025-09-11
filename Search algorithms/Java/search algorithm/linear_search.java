caa5e606d17d0f4a673e03155dd946dcc57e1005
Lucas Grimm Aguilera <Lucas230208@icloud.com>
2025-09-11 19:13:16 +0200

    Update and rename linear_search to linear_search.java

---
+++ Search algorithms/Java/search algorithm/linear_search.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    static boolean linear_search(int[] numbers, int target)
+    {
+        for (int i = 0; i < numbers.length; i++)
+        {
+            if (numbers[i] == target)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
